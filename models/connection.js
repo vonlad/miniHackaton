8a5bf0db8b6b10233f917bd24b3c7ac156af3441
Jean-Baptiste LARRIVIERE <76693792+jb-larriviere@users.noreply.github.com>
2021-10-08 11:06:00 +0200

    update mogno connection string

--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ var options = {
   useNewUrlParser: true,
 }
 
-mongoose.connect('mongodb+srv://admin:UEG6h4itcrphgbig@cluster0.r0nfd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://admin:UEG6h4itcrphgbig@cluster0.r0nfd.mongodb.net/morningnews?retryWrites=true&w=majority',
   options,
   function (err) {
     console.log(err);
